44e86021d5c37b4ca7181d2545b3e7bd00b94702
jesse.chung9727@gmail.com <jesse.chung9727@gmail.com>
2023-07-10 20:19:15 -0400

    WIP: working on script for movie app

--- movie-script.js
+++ movie-script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+/** When the form has is submitted, capture the values for each of the
+inputs and append them do the DOM along with a button to remove each
+title and rating from the DOM.
+When the button to remove is clicked, remove each title and rating from
+the DOM.*/
+
+
+//grab the two values, title and rating
+//create a button to remove each title and rating (add eventListener)
+//append to DOM
+
+
+
+function addMovie(evt){
+  let formTitle = $('#form-title').val();
+  let formRating = $('#form-rating').val();
+
+  let $titleAndRating = $("<div>");
+  let $button = $("<button>").text('Remove');
+
+
+  $('#movie-container').append($titleAndRating);
+
+  $titleAndRating.append(formTitle, formRating, $button);
+  evt.preventDefault();
+}
+
+$('#form-submit').on('click', addMovie)
+

44b2b746acc9e4a740806819bd9c7f3d51733bc3
Stuart Fleisher <stufleisher@gmail.com>
2023-07-10 20:54:01 -0400

    WIP: working on sort functions

--- movie-script.js
+++ movie-script.js
@@ -14,18 +14,45 @@ the DOM.*/
 
 
 function addMovie(evt){
-  let formTitle = $('#form-title').val();
-  let formRating = $('#form-rating').val();
+  let formTitleData = $('#form-title').val();
+  let formRatingData = $('#form-rating').val();
 
-  let $titleAndRating = $("<div>");
-  let $button = $("<button>").text('Remove');
+  let $movieTitle = $('<span>')
+      .text(formTitleData)
+      .addClass('title');
+  let $movieRating = $('<span>')
+      .text(formRatingData)
+      .addClass('rating')
+      .css('margin-left','10px');
+  let $button = $("<button>")
+      .addClass('movie-delete')
+      .text('Remove');
 
+  let $titleAndRating = $("<div>").addClass('movie');
 
   $('#movie-container').append($titleAndRating);
 
-  $titleAndRating.append(formTitle, formRating, $button);
+  $titleAndRating.append( $movieTitle, $movieRating, $button);
   evt.preventDefault();
 }
 
-$('#form-submit').on('click', addMovie)
+function removeMovie(evt){
+  $(evt.target).parent().remove();
+}
 
+$('#movie-container').on('click', '.movie-delete', removeMovie);
+$('#form-submit').on('click', addMovie);
+
+//SORT BY TITLE
+  //get all movie title data
+  //sort title data
+  //re-render the DOM
+
+function sortByTitle() {
+  let $movies = $('.movie');
+  let $titles = $('.title');
+
+  let titlesArray = $titles.toArray();
+
+
+}
